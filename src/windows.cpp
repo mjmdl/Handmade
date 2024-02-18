d4b3c50c23ca72c93d369e2ebcae07d06d7712da
Matheus Maidel <matheus.maidel@gmail.com>
2024-02-18 17:06:57 -0300

    windows window

--- src/windows.cpp
+++ src/windows.cpp
@@ -1,7 +1,65 @@
 #include <windows.h>
 
+namespace win32 {
+
+static LRESULT CALLBACK window_procedure(HWND window, UINT message, WPARAM w_param, LPARAM l_param)
+{
+	switch (message) {
+	case WM_CLOSE:
+		DestroyWindow(window);
+		return 0;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+
+	default:
+		return DefWindowProc(window, message, w_param, l_param);
+	}
+}
+
+} // win32
+
 int APIENTRY WinMain(HINSTANCE instance, HINSTANCE previous_instance, LPSTR command_line, int command_show)
 {
-	MessageBoxA(NULL, "Hello, World!", "Message", (MB_ICONINFORMATION | MB_OK));
-	return 0;
+	WNDCLASSA window_class = {};
+	window_class.style = (CS_OWNDC | CS_HREDRAW | CS_VREDRAW);
+	window_class.lpfnWndProc = win32::window_procedure;
+	window_class.hInstance = instance;
+	window_class.lpszClassName = "Handmade_Window_Class";
+
+	if (!RegisterClassA(&window_class))
+		return FALSE;
+
+	int screen_width = GetSystemMetrics(SM_CXSCREEN);
+	int screen_height = GetSystemMetrics(SM_CYSCREEN);
+
+	HWND window = CreateWindowExA(
+		0, window_class.lpszClassName, "Handmade", (WS_OVERLAPPEDWINDOW | WS_VISIBLE),
+		(screen_width / 8), (screen_height / 8), (screen_width * 3 / 4), (screen_height * 3 / 4),
+		NULL, NULL, instance, NULL);
+
+	if (!window)
+		return FALSE;
+
+	HDC device_context = GetDC(window);
+
+	if (!device_context)
+		return FALSE;
+
+	bool keep_running = true;
+	MSG message = {};
+
+	while (keep_running) {
+		while (PeekMessageA(&message, NULL, 0, 0, PM_REMOVE)) {
+			if (message.message == WM_QUIT)
+				keep_running = false;
+			else {
+				TranslateMessage(&message);
+				DispatchMessage(&message);
+			}
+		}
+	}
+
+	return (int)message.wParam;
 }
