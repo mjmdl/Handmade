13185cd54104fff28a6971ca856ecb8f6724ecde
Matheus Maidel <matheus.maidel@gmail.com>
2024-02-18 17:23:30 -0300

    render weird gradient

---
+++ src/handmade.cpp
@@ -0,0 +1,29 @@
+#include <stdint.h>
+
+namespace hm {
+
+struct FrameBuffer {
+	uint32_t width;
+	uint32_t height;
+	uint32_t pitch;
+	void *memory;
+};
+
+static void render_gradient(FrameBuffer *frame, uint8_t x_offset, uint8_t y_offset)
+{
+	uint8_t *row = (uint8_t *)frame->memory;
+	
+	for (uint32_t y = 0; (y < frame->height); y++) {
+		uint32_t *pixel = (uint32_t *)row;
+		uint8_t green = (uint8_t)(y + y_offset);
+
+		for (uint32_t x = 0; (x < frame->width); x++) {
+			uint8_t blue = (uint8_t)(x + x_offset);
+			*pixel++ = ((green << 8) | blue);
+		}
+		
+		row += frame->pitch;
+	}
+}
+
+} // hm
--- src/windows.cpp
+++ src/windows.cpp
@@ -1,6 +1,54 @@
+#include "handmade.cpp"
 #include <windows.h>
 
-namespace win32 {
+namespace win {
+
+struct WindowBuffer {
+	uint8_t pixel_bytes;
+	hm::FrameBuffer frame;
+	BITMAPINFO bitmap_info;
+};
+
+struct WindowSize {
+	uint32_t width;
+	uint32_t height;
+};
+
+static WindowSize get_window_size(HWND window)
+{
+	RECT rect;
+	GetClientRect(window, &rect);
+
+	WindowSize size;
+	size.width = (rect.right - rect.left);
+	size.height = (rect.bottom - rect.top);
+	return size;
+}
+
+static void resize_window_buffer(WindowBuffer *buffer, uint32_t width, uint32_t height)
+{
+	uint8_t pixel_bytes = (buffer->pixel_bytes = 4);
+	buffer->frame.width = width;
+	buffer->frame.height = height;
+	buffer->frame.pitch = (pixel_bytes * width);
+
+	if (buffer->frame.memory)
+		VirtualFree(buffer->frame.memory, 0, MEM_RELEASE);
+	buffer->frame.memory = VirtualAlloc(NULL, (width * height * pixel_bytes), (MEM_COMMIT | MEM_RESERVE), PAGE_READWRITE);
+
+	buffer->bitmap_info.bmiHeader.biSize = sizeof(buffer->bitmap_info.bmiHeader);
+	buffer->bitmap_info.bmiHeader.biWidth = width;
+	buffer->bitmap_info.bmiHeader.biHeight = (height * -1);
+	buffer->bitmap_info.bmiHeader.biPlanes = 1;
+	buffer->bitmap_info.bmiHeader.biBitCount = (pixel_bytes * 8);
+	buffer->bitmap_info.bmiHeader.biCompression = BI_RGB;
+}
+
+static inline void present_window_buffer(WindowBuffer *buffer, HDC context, uint32_t width, uint32_t height)
+{
+	StretchDIBits(context, 0, 0, width, height, 0, 0, buffer->frame.width, buffer->frame.height,
+				  buffer->frame.memory, &buffer->bitmap_info, DIB_RGB_COLORS, SRCCOPY);
+}
 
 static LRESULT CALLBACK window_procedure(HWND window, UINT message, WPARAM w_param, LPARAM l_param)
 {
@@ -18,13 +66,13 @@ static LRESULT CALLBACK window_procedure(HWND window, UINT message, WPARAM w_par
 	}
 }
 
-} // win32
+} // win
 
 int APIENTRY WinMain(HINSTANCE instance, HINSTANCE previous_instance, LPSTR command_line, int command_show)
 {
 	WNDCLASSA window_class = {};
 	window_class.style = (CS_OWNDC | CS_HREDRAW | CS_VREDRAW);
-	window_class.lpfnWndProc = win32::window_procedure;
+	window_class.lpfnWndProc = win::window_procedure;
 	window_class.hInstance = instance;
 	window_class.lpszClassName = "Handmade_Window_Class";
 
@@ -47,6 +95,11 @@ int APIENTRY WinMain(HINSTANCE instance, HINSTANCE previous_instance, LPSTR comm
 	if (!device_context)
 		return FALSE;
 
+	win::WindowBuffer window_buffer = {};
+	win::resize_window_buffer(&window_buffer, 1440, 810);
+	uint8_t render_x_offset = 0;
+	uint8_t render_y_offset = 0;
+	
 	bool keep_running = true;
 	MSG message = {};
 
@@ -59,6 +112,11 @@ int APIENTRY WinMain(HINSTANCE instance, HINSTANCE previous_instance, LPSTR comm
 				DispatchMessage(&message);
 			}
 		}
+
+		hm::render_gradient(&window_buffer.frame, render_x_offset++, render_y_offset++);		
+
+		win::WindowSize window_size = win::get_window_size(window);
+		win::present_window_buffer(&window_buffer, device_context, window_size.width, window_size.height);
 	}
 
 	return (int)message.wParam;
